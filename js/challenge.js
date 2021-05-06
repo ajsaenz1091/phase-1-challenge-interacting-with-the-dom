13451f83a1a441efeff034b0c9f9c513ca3d09e9
Alberto <ajsaenz1091@gmail.com>
2021-05-06 02:16:38 -0400

    commit tree 97e4563d93c11caef737a5f023ac2cd54925e1c6

---
+++ js/challenge.js
@@ -0,0 +1,47 @@
+//* As a user, I should see the timer increment every second once the page has loaded.
+//* As a user, I can manually increment and decrement the counter using the plus and minus buttons.
+//* As a user, I can 'like' an individual number of the counter. I should see the count of the number of 'likes' associated with that number displayed.
+
+// document.addEventListener('DOMContentLoaded', () =>{
+
+//      const counter = document.getElementById('counter'); // h1 element grabbed
+//      const minus = document.getElementById('minus');
+//      const plus = document.getElementById('plus');
+//      const heart = document.getElementById('heart');
+//      const pause = document.getElementById('pause');
+
+//      const input = document.getElementById('comment-input')
+//      const submit = document.getElementById('submit');
+
+//      const ul = document.createElement('ul'); // created an empty ul tag <ul></ul>
+
+//      let seconds = 0;
+
+//      function incrementSeconds(){
+//         seconds += 1;
+//         counter.innerHTML = seconds;
+//      }
+
+//      function manualIncrement(){
+//             seconds += 1;
+//             counter.innerHTML = seconds
+//      }
+
+//      function manualDecrement(){
+//         if(seconds >  0){
+//             seconds -= 1;
+//             counter.innerHTML = seconds
+//         }
+//      }
+
+//      plus.addEventListener('click', manualIncrement)
+//      minus.addEventListener('click', manualDecrement)
+
+     
+
+//     //  setInterval(incrementSeconds,1000);
+
+
+
+
+// })
